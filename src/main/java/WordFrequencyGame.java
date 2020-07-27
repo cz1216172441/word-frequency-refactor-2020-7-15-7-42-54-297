79ac7aaf23cd8a28a768335d5d544b20b998b36e
CHENRO6 <chenro6@oocl.com>
2020-07-27 15:13:49 +0800

    [Word Frequency Refactoring][Roy] refactor: extract getWordFrequencyMap and sortWordsFrequencyDesc method in class WordFrequencyGame

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,22 +5,9 @@ public class WordFrequencyGame {
     public String wordStatistic(String sentence) {
         String[] words = sentence.split("\\s+");
         try {
-            Map<String, Integer> wordMap = new HashMap<>();
-            for (String word : words) {
-                if (!wordMap.containsKey(word)) {
-                    wordMap.put(word, 1);
-                } else {
-                    wordMap.put(word, wordMap.get(word) + 1);
-                }
-            }
+            Map<String, Integer> wordMap = getWordFrequencyMap(words);
 
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-                Input input = new Input(entry.getKey(), entry.getValue());
-                list.add(input);
-            }
-
-            list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+            List<Input> list = sortWordsFrequencyDesc(wordMap);
 
             StringJoiner joiner = new StringJoiner("\n");
             for (Input w : list) {
@@ -32,6 +19,29 @@ public class WordFrequencyGame {
             return "Calculate Error";
         }
 
+    }
+
+    private List<Input> sortWordsFrequencyDesc(Map<String, Integer> wordMap) {
+        List<Input> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue());
+            list.add(input);
+        }
+
+        list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        return list;
+    }
+
+    private Map<String, Integer> getWordFrequencyMap(String[] words) {
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            if (!wordMap.containsKey(word)) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, wordMap.get(word) + 1);
+            }
+        }
+        return wordMap;
     }
 
 }
